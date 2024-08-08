0f69573e1ffac6c993f8d330291e3a865e7af260
aziz9301 <78246950+aziz9301@users.noreply.github.com>
2024-08-08 21:03:06 +0700

    json lagi

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  reactStrictMode: true,
 };
+// const nextConfig = {
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+// };
 
 export default nextConfig;
